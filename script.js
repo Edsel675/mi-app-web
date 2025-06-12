d1d21b5a42a3019a9c3a0ff6269ad1bd3eafa1f9
Edsel De Jesus Cisneros Bautista <111032109+Edsel675@users.noreply.github.com>
2025-06-11 22:20:32 -0600

    Conecto frontend con backend

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    
-    document.getElementById('message').textContent = `¡Gracias por registrarte, ${name}!`;
+
+    fetch('https://mi-app-backend-zp4j.onrender.com/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Respuesta del backend:', data);
+        document.getElementById('message').textContent = `¡Gracias por registrarte, ${name}!`;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('message').textContent = 'Ocurrió un error al registrar.';
+    });
 });
